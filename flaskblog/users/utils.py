b4fb976ca827cc36509df722950f980c0aacf690
Eric Trimble <ectrimble@hotmail.com>
2018-05-09 19:11:43 -0400

    More blueprint conversions.  Also converted configuration values, should be reading env variables for most of the settings now.

--- flaskblog/users/utils.py
+++ flaskblog/users/utils.py
@@ -1,8 +1,8 @@
 import secrets
 import os
 from flask_mail import Message
-from flaskblog import mail, app
-from flask import url_for
+from flaskblog import mail
+from flask import url_for, current_app
 from PIL import Image
 
 
@@ -10,7 +10,7 @@ def save_picture(form_picture):
     random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
-    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+    picture_path = os.path.join(current_app.root_path, 'static/profile_pics', picture_fn)
 
     output_size = (125, 125)
     i = Image.open(form_picture)
