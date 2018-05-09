bbc5a5d95b8cb629cf5ac3acf2aa77d5e0d2e32c
Eric Trimble <ectrimble@hotmail.com>
2018-05-09 18:50:06 -0400

    Big update, converted the project over to use flask blueprints, this involved removing some files while adding in others.  We also updated all the routes used by the url_for method to include package names now (e.g new_post is now posts.new_post) as well as migrating all route code into sub packages (or blueprints).

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -11,7 +11,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
 app.config['MAIL_SERVER'] = "smtp.googlemail.com"
 app.config['MAIL_PORT'] = 587
@@ -20,4 +20,12 @@ app.config['MAIL_USERNAME'] = os.environ.get('FLASK_EMAIL_USER')
 app.config['MAIL_PASSWORD'] = os.environ.get('FLASK_EMAIL_PASS')
 mail = Mail(app)
 
-from flaskblog import routes
+
+from flaskblog.users.routes import users
+app.register_blueprint(users)
+
+from flaskblog.posts.routes import posts
+app.register_blueprint(posts)
+
+from flaskblog.main.routes import main
+app.register_blueprint(main)
