388d019368864347acd451636417f56757ae0dc8
Eric Trimble <ectrimble@hotmail.com>
2018-06-23 01:44:56 -0400

    Updating configuration settings for mobility

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -2,6 +2,9 @@ import os
 
 
 class Config(object):
+    ENV = os.environ.get('FLASK_BB_ENV', 'production')
+    DEBUG = os.environ.get('FLASK_BB_DEBUG', False)
+    TESTING = os.environ.get('FLASK_BB_TESTING', False)
     SECRET_KEY = os.environ.get('FLASK_SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('FLASK_DB_URI')
     MAIL_SERVER = "smtp.googlemail.com"
@@ -9,3 +12,4 @@ class Config(object):
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('FLASK_EMAIL_USER')
     MAIL_PASSWORD = os.environ.get('FLASK_EMAIL_PASS')
+    HOST = os.environ.get('FLASK_HOST', '0.0.0.0')
